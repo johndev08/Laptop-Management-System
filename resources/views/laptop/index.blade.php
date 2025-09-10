7c872d37a3500e740c21e3c363f0624a3f0eb0e6
John Lloyd Mortel <johnlloydmortel.dev@gmail.com>
2025-09-10 11:40:40 +0800

    add style to the table index

--- resources/views/laptop/index.blade.php
+++ resources/views/laptop/index.blade.php
@@ -9,26 +9,29 @@
     <title>Laptop Management System</title>
 </head>
 
-<body>
-    <h1>Laptop Management System</h1>
-    <a href="{{ route('laptop.create') }}">Add laptop</a>
-    <table class="min-w-xl">
-        <tr>
-            <th>Name</th>
-            <th>Processor</th>
-            <th>Storage</th>
-            <th>Operating System</th>
-        </tr>
-
-        @foreach ($laptop as $laptops)
-            <tr onclick="window.location='{{ route('laptop.show', $laptops->id) }}'" class="hover:bg-gray-300 cursor-pointer">
-                <td>{{ $laptops->laptopname }}</td>
-                <td>{{ $laptops->processor }}</td>
-                <td>{{ $laptops->storage }}</td>
-                <td>{{ $laptops->operatingsystem }}</td>
+<body >
+    <div class="max-w-screen ">
+        <nav class="my-5 max-w-3xl flex mx-auto justify-between">
+            <h1 class="font-light text-2xl">Laptop Management System</h1>
+            <a href="{{ route('laptop.create') }}" class="bg-blue-500 text-white rounded px-2 py-1 hover:bg-blue-700 transition">Add laptop</a>
+        </nav>
+        <table class="min-w-xl mx-auto mt-10">
+            <tr class="bg-gray-500 text-white">
+                <th class="py-2">Name</th>
+                <th class="py-2">Processor</th>
+                <th class="py-2">Storage</th>
+                <th class="py-2">Operating System</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($laptop as $laptops)
+                <tr onclick="window.location='{{ route('laptop.show', $laptops->id) }}'" class="hover:bg-gray-300 cursor-pointer not-even:bg-gray-200">
+                    <td class="py-2">{{ $laptops->laptopname }}</td>
+                    <td class="py-2">{{ $laptops->processor }}</td>
+                    <td class="py-2">{{ $laptops->storage }}</td>
+                    <td class="py-2">{{ $laptops->operatingsystem }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 
 </html>
