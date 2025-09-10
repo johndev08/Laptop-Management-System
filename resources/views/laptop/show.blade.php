ca5eae7fa09a241a2b10f14d6fe6833461025035
John Lloyd Mortel <johnlloydmortel.dev@gmail.com>
2025-09-10 12:00:11 +0800

    add style to the show.blade

--- resources/views/laptop/show.blade.php
+++ resources/views/laptop/show.blade.php
@@ -10,17 +10,31 @@
 </head>
 
 <body>
-<p>{{ $laptop->laptopname }}</p>
-<p>{{ $laptop->processor }}</p>
-<p>{{ $laptop->storage }}</p>
-<p>{{ $laptop->operatingsystem }}</p>
-<a href="{{ route('laptop.edit', $laptop->id) }}">Edit</a>
-<a href="{{ route('laptop.index') }}">Cancel</a>
-<form action="{{ route('laptop.destroy', $laptop->id) }}" method="post">
-    @csrf
-    @method('DELETE')
-    <input type="submit" value="Delete">
-</form>
+    <div class="max-w-screen flex min-h-screen">
+        <div class="min-w-sm bg-gray-300 rounded-md p-5 m-auto">
+            <p class="text-center text-3xl font-bold">{{ $laptop->laptopname }}</p>
+            <div class="py-3">
+                <p><b>Processor:</b> {{ $laptop->processor }}</p>
+                <p><b>Storage:</b> {{ $laptop->storage }}</p>
+                <p><b>Operaring System:</b> {{ $laptop->operatingsystem }}</p>
+            </div>
+            <div class="inline-flex">
+                <a class="bg-green-700 hover:bg-green-800 text-white px-2 py-1 rounded"
+                    href="{{ route('laptop.edit', $laptop->id) }}">Edit</a>
+            </div>
+            <div class="inline-flex">
+                <a class="bg-blue-700 hover:bg-blue-800 text-white px-2 py-1 rounded"
+                    href="{{ route('laptop.index') }}">Cancel</a>
+            </div>
+            <form class="inline-flex" action="{{ route('laptop.destroy', $laptop->id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <input type="submit" class="bg-red-700 hover:bg-red-800 text-white px-2 py-1 rounded" value="Delete">
+            </form>
+        </div>
+    </div>
+
+
 </body>
 
 </html>
